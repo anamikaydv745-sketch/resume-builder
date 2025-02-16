dcb49a807f34d0173a46d0eddf95665cb49780e3
Syed Abbas <syedabbassyed786@gmail.com>
2025-02-16 23:19:20 +0530

    removed all the import related files to stick with the core functionality and updated redux state design

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,32 +1,12 @@
+
 import { Link } from "react-router";
-import { FileDropZone } from "../components/FileDropZone";
-import { useState } from "react";
-import { parseResumeSections } from "../components/ParseResumeSections";
-import { extractTextFromDOCX } from "../components/DocxExtractor";
 
 export const Landing = () => {
-    const [resumeText, setResumeText] = useState("");
-    const [resumeSections, setResumeSections] = useState({});
-
-    const handleFileUpload = async (file) => {
-        
-        let extractedText = "";
-
-        extractedText = await extractTextFromDOCX(file);
-
-        setResumeText(extractedText);
-        setResumeSections(parseResumeSections(extractedText));
-
-        console.log(extractedText)
-        console.log(parseResumeSections(extractedText))
-    };
+    
     return (
         <div>
             <h1>Welcome to the Landing Page</h1>
             <Link to="/editor">Go to Editor</Link>
-            <FileDropZone onFileUpload={handleFileUpload}/>
-            <div>{resumeText}</div>
-            <div>{JSON.stringify(resumeSections)}</div>
         </div>
     );
-}
+};
