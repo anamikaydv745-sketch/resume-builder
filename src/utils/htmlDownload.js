d182c9f2605a739a9baefaa2f81bc7d591e86f37
Syed Abbas <syedabbassyed786@gmail.com>
2025-03-26 01:59:10 +0530

    changed the pdf file name

--- src/utils/htmlDownload.js
+++ src/utils/htmlDownload.js
@@ -10,7 +10,7 @@ export const handleDownload = async (setIsDownloading) => {
   
     const options = {
       margin: 0,
-      filename: 'Syed_Abbas_Resume.pdf',
+      filename: 'Resume.pdf',
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: { scale: 2, useCORS: true },
       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
